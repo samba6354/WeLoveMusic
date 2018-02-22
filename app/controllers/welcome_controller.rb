16c765fc29a3bb6537c11db47a4a6eaaba0fe7f0
samba6354 <samba6354@gmail.com>
2018-02-22 09:03:43 -0500

    project committed

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,39 @@
+class WelcomeController<ApplicationController
+
+def home
+end
+
+def songs
+end
+
+def gaali
+end
+
+def merise
+end
+
+def kothaga
+end
+
+def rayyi
+end
+
+def yenthara
+end
+
+def teluse
+end
+
+def malli
+end
+
+def nekalla
+end
+
+def oosupodu
+end
+
+def adiga
+end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+root 'welcome#home'
+
+get 'songs', to: 'welcome#songs'
+get 'gaali', to: 'welcome#gaali'
+get 'merise', to: 'welcome#merise'
+get 'kothaga', to: 'welcome#kothaga'
+get 'rayyi', to:'welcome#rayyi'
+get 'yenthara', to:'welcome#yenthara'
+get 'teluse', to:'welcome#teluse'
+get 'malli', to:'welcome#malli'
+get 'nekalla', to:'welcome#nekalla'
+get 'oosupodu', to:'welcome#oosupodu'
+get 'adiga', to:'welcome#adiga'
+end
