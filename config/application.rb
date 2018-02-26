0a2e12b2a8a4114960c4ade78dbbca31ddffc3e7
Samba <samba6354@gmail.com>
2018-02-26 13:45:21 -0500

    production rb line added

--- config/application.rb
+++ config/application.rb
@@ -13,10 +13,6 @@ module TestMusic
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.cache_classes = true
-    config.serve_static_assets = true
-    config.assets.compile = true
-    config.assets.digest = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
