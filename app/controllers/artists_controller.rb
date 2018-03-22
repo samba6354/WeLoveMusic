9e0f69470141853b4a653577e6ee32cf75c4adac
Samba <samba6354@gmail.com>
2018-03-22 16:25:32 -0400

    artists added

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,24 @@
+class ArtistsController<ApplicationController
+
+def dsp
+end
+
+def anirudh
+end
+
+def thaman
+end
+
+def anoop
+end
+
+def ar_rehman
+end
+
+def ilayaraja
+end
+
+def manisharma
+end
+
+end
